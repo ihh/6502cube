52b74ee6f8b4775f1babb6696575b158f1b5d71c
Ian Holmes <ihh@berkeley.edu>
2018-07-07 10:33:38 -0700

    src/mersenne-twister.js

--- README.md
+++ README.md
@@ -1,7 +1,8 @@
 # 6502wars
-Whitepaper describing 6502-based cellular automata
+Whitepaper and code for 6502-based cellular automata
+c.f. Core War, Avida
 
 Code acknowledgements:
-- Emulator: https://github.com/Torlus/6502.js
-- Assembler: http://www.masswerk.at/6502/
-
+- 6502 emulator: https://github.com/Torlus/6502.js
+- 6502 assembler: http://www.masswerk.at/6502/
+- Mersenne Twister: https://github.com/boo1ean/mersenne-twister
--- src/chunk.js
+++ src/chunk.js
@@ -6,7 +6,13 @@ class Chunk {
     this.xSize = config.xSize || config.size || 1
     this.ySize = config.ySize || config.size || 1
     this.zSize = config.zSize || config.size || 1
-    // More to go here
+    this.state = new Array(this.xSize).fill(0).map (function() {
+      return new Array(this.ySize).fill(0).map (function() {
+        return new Array(this.zSize).fill(0).map (function() {
+          return new Uint8Array (256)
+        })
+      })
+    })
   }
 }
 
